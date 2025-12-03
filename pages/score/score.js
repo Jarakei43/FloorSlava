739d6c2219bfad2e3772f2fe73f9806bb97c23f4
Jarakei43 <134204371+Jarakei43@users.noreply.github.com>
2025-12-03 12:56:05 +0600

    Update score.js

--- pages/score/score.js
+++ pages/score/score.js
@@ -1,4 +1,8 @@
 const resultData = sessionStorage.getItem("gameResult");
+const userData = sessionStorage.getItem("user")
+const user = userData ? JSON.parse(userData) : null;
 const result = resultData ? JSON.parse(resultData) : null;
 const score = document.querySelector(".score-box");
+const title = document.querySelector(".score-title")
 score.innerHTML = result.score;
+title.innerHTML = `${user.name} Your Score Is`
